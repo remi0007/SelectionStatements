e972bac0b874e5a03ff6c6032e9d3fc1c8aa9ab9
Remi Douah <remidouah@gmail.com>
2020-10-26 21:35:26 -0500

    Added Switch

---
+++ Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite schoocl subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is a tough subject!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is a fun subject!");
+                    break;
+
+                default:
+              
+                    Console.WriteLine($" Oh really! I have not taken that subject before. {subject} sounds fun!S");
+                    break;
+            }
+        }
+    }
+}
